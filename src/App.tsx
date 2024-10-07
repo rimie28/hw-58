f4980657c5fa2a6355a241c8ec9270454f46853c
Meerim Maratova <maratovameerim@gmail.com>
2024-10-07 16:15:55 +0600

    added Close button and OnClose function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-
-
+import { useState } from 'react';
+import Modal from './Components/Modal/Modal.tsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
 
+  const [showModal, setShowModal] = useState(false);
+
+  const CloseModal = () => {
+    setShowModal(false);
+  }
+
+  const OpenModal = () => {
+    setShowModal(true);
+  }
+
   return (
     <>
+      <div>
+        <button onClick={OpenModal}>Open Modal</button>
 
+        <Modal show={showModal} onClose={CloseModal}></Modal>
+
+      </div>
     </>
   )
 };
---
+++ src/Components/Modal/Modal.tsx
@@ -0,0 +1,28 @@
+import Backdrop from '../Backdrop/Backdrop.tsx';
+import React from 'react';
+
+interface Props {
+  show: boolean;
+  onClose: () => void;
+}
+
+const Modal:React.FC<Props> = ({show, onClose}) => {
+  return (
+    <>
+      <Backdrop show={show}/>
+      <div className="modal show" style={{display: show ? 'block' : 'none'}}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5">Modal title</h1>
+              <button type="button" className="btn-close" aria-label="Close" onClick={onClose}></button>
+            </div>
+            Modal Content
+          </div>
+        </div>
+      </div>
+    </>
+  )
+};
+
+export default Modal;
